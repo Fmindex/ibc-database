6be72c48248cfdd0d745effa10dc954fd948c420
focusjk <jiraphat.khupanit@gmail.com>
2018-09-04 00:34:36 +0700

    kord

--- src/container/profile.js
+++ src/container/profile.js
@@ -9,52 +9,32 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
-
-const cookies = new Cookies();
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class Profile extends Component {
-
   render = () => {
     return (
       <div className="col-xs-12" style={{ overflow: 'scroll', height: '100%' }}>
         <Table>
           <TableBody displayRowCheckbox={false}>
-            <TableRow>
-              <TableRowColumn style={{ fontSize: 16, width: 150 }} >Student ID :</TableRowColumn>
-              <TableRowColumn style={{ fontSize: 16, }} >{this.props.userInfo.student_id}</TableRowColumn>
-            </TableRow>
-
-            <TableRow>
-              <TableRowColumn style={{ fontSize: 16, width: 150 }} >Name :</TableRowColumn>
-              <TableRowColumn style={{ fontSize: 16, }} >{this.props.userInfo.name}</TableRowColumn>
-            </TableRow>
-
-            <TableRow>
-              <TableRowColumn style={{ fontSize: 16, width: 150 }} >Citizen ID :</TableRowColumn>
-              <TableRowColumn style={{ fontSize: 16, }} >{this.props.userInfo.citizen_id}</TableRowColumn>
-            </TableRow>
-
-            <TableRow>
-              <TableRowColumn style={{ fontSize: 16, width: 150 }} >Faculty :</TableRowColumn>
-              <TableRowColumn style={{ fontSize: 16, }} >{this.props.userInfo.faculty_name}</TableRowColumn>
-            </TableRow>
-
-            <TableRow>
-              <TableRowColumn style={{ fontSize: 16, width: 150 }} >Department :</TableRowColumn>
-              <TableRowColumn style={{ fontSize: 16, }} >{this.props.userInfo.department_name}</TableRowColumn>
-            </TableRow>
-
-            <TableRow>
-              <TableRowColumn style={{ fontSize: 16, width: 150 }} >Program :</TableRowColumn>
-              <TableRowColumn style={{ fontSize: 16, }} >{this.props.userInfo.program}</TableRowColumn>
-            </TableRow>
-
-            <TableRow>
-              <TableRowColumn style={{ fontSize: 16, width: 150 }} >Year :</TableRowColumn>
-              <TableRowColumn style={{ fontSize: 16, }} >{this.props.userInfo.year}</TableRowColumn>
-            </TableRow>
+            {this.props.columnData.map(item => (
+              <TableRow>
+                <TableRowColumn style={{ fontSize: 16, width: 150 }}>{item.text} :</TableRowColumn>
+                <TableRowColumn style={{ fontSize: 16 }}>
+                  {this.props.userInfo[item.value] ? this.props.userInfo[item.value] : undefined}
+                </TableRowColumn>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
+
+        <div className="row" style={{ marginLeft: '64px' }}>
+          <RaisedButton
+            label="Edit"
+            primary={false}
+            onClick={() => this.props.onModeChange('Edit')}
+          />
+        </div>
       </div>
     );
   };
--- src/pages/DashboardStudent.js
+++ src/pages/DashboardStudent.js
@@ -4,14 +4,13 @@ import { Card, CardActions, CardHeader, CardMedia, CardText, CardTitle } from 'm
 import { List, ListItem } from 'material-ui/List';
 import React, { Component } from 'react';
 import axios from 'axios';
-import Cookies from 'universal-cookie';
 
 import ActionDescription from 'material-ui/svg-icons/action/description';
 import ActionPayment from 'material-ui/svg-icons/action/payment';
 import ActionSchedule from 'material-ui/svg-icons/action/schedule';
 import ActionUser from 'material-ui/svg-icons/action/face';
 import ActionToc from 'material-ui/svg-icons/action/toc';
-import Grade from '../container/grade';
+// import Grade from '../container/grade';
 import HomePage from './HomePage';
 import Request from '../container/request';
 import Payment from '../container/payment';
@@ -20,88 +19,186 @@ import SearchPanel from '../container/search_panel';
 import SocialSchool from 'material-ui/svg-icons/social/school';
 import Table from '../container/new_table';
 import Divider from 'material-ui/Divider/Divider';
+import Grade from '../container/addGrade';
 
 class DashboardStudent extends Component {
-
-  cookies = new Cookies();
-
   state = {
-    pages: [
-      { name: 'Profile', detail: 'View your student profile' },
-      { name: 'Schedule', detail: 'View your course table' },
-      { name: 'Course', detail: 'Add / Remove / Withdraw your courses' },
-      { name: 'Grade', detail: 'View your grade' },
-      { name: 'Request', detail: 'Request a transcript ans so on' },
-      { name: 'Payment', detail: 'Manage your payment' },
-    ],
-    selectedPage: { name: 'Profile', detail: 'View your student profile' },
     userInfo: {},
-    studentCourses: [],
+    mode: '',
+    list: [
+      {
+        intNo: '',
+        fname: 'ekkalak',
+        lname: 'leelasornchai',
+        nname: 'fm',
+        address: '',
+        road: '',
+        subdistrict: '',
+        district: '',
+        province: '',
+        PostCode: '',
+        homeNo: '',
+        telNo: '',
+        telNo2: '',
+        Email: '',
+        workplace: '',
+        gender: '',
+        position: '',
+      },
+      {
+        intNo: '',
+        fname: 'jiraphat',
+        lname: 'khupanit',
+        nname: 'focus',
+        address: '',
+        road: '',
+        subdistrict: '',
+        district: '',
+        province: '',
+        PostCode: '',
+        homeNo: '',
+        telNo: '',
+        telNo2: '',
+        Email: '',
+        workplace: '',
+        gender: '',
+        position: '',
+      },
+    ],
+    resultList: [],
+    columnData: [
+      { value: 'intNo', text: 'Intania' },
+      { value: 'fname', text: 'FirstName' },
+      { value: 'lname', text: 'Lastname' },
+      { value: 'nname', text: 'Nickname' },
+      { value: 'address', text: 'Address' },
+      { value: 'road', text: 'Street' },
+      { value: 'subdistrict', text: 'Subdistrict' },
+      { value: 'district', text: 'District' },
+      { value: 'province', text: 'Province' },
+      { value: 'PostCode', text: 'Zipcode' },
+      { value: 'homeNo', text: 'Home nu' },
+      { value: 'telNo', text: 'Tel.' },
+      { value: 'telNo2', text: 'Tel.2' },
+      { value: 'Email', text: 'Email' },
+      { value: 'workplace', text: 'Work' },
+      { value: 'gender', text: 'Gender' },
+      { value: 'position', text: 'Position Player' },
+      // { value: 'status', text: 'status' },
+    ],
   };
 
-  componentWillMount() {
-    this.getStudentCourses();
-    this.getUserInfo();
-  }
+  componentDidMount() {
+    // axios.get('http://localhost:3000/course/all').then(res => {
 
-  changePage = (nextPage) => {
+    // 	this.setState({
+    // 		courses: res.data.courses
+    // 	}, () => {
+    // 		let pickSection = [];
+    // 		res.data.courses.map((course, index) => {
+    // 			pickSection.push(course.sections[0].section_id);
+    // 		});
+    // 		this.setState({
+    // 			pickSection: pickSection
+    // 		}, () => this.updateResult());
+    // 	});
+    // });
     this.setState({
-      selectedPage: this.state.pages[nextPage],
-    })
+      resultList: this.state.list,
+    });
   }
 
-  getStudentCourses = () => {
-    axios.get('http://localhost:3000/student/course/all?token=' + this.cookies.get('token')).then(res => {
-      this.setState({
-        studentCourses: res.data.courses,
-      });
+  handleSearch = data => {
+    const keyword = data.target.value;
+    let temp = [];
+    temp = this.state.list.filter(item => {
+      return (
+        item.fname.includes(keyword) || item.lname.includes(keyword) || item.nname.includes(keyword)
+      );
+    });
+    this.setState({
+      resultList: temp,
+    });
+  };
+  handleListClicked = item => {
+    this.setState({
+      userInfo: item,
+      mode: 'Profile',
     });
   };
 
-  getUserInfo = () => {
-    axios.get(`http://localhost:3000/student/info?token=${this.cookies.get('token')}`).then(res => {
-      if(res.data.userInfo) {
-        this.setState({
-          userInfo: res.data.userInfo,
-        });
-      }
+  handleModeChanged = mode => {
+    this.setState({
+      mode: mode,
     });
-  }
+  };
 
+  handleEdit = newInfo => {
+    this.setState({
+      userInfo: {
+        ...this.state.userInfo,
+        ...newInfo,
+      },
+      mode: 'Profile',
+    });
+  };
   render = () => {
+    const { userInfo, mode, list, resultList, columnData } = this.state;
     return (
       <div className="row" style={{ height: '80%', fontSize: '24px', marginTop: '50px' }}>
-        <div className="col-md-3 sidenav" style={{ height: '94vh', paddingLeft: '32px' }}>
-          <div style={{ width: '100%', textAlign: 'center', paddingTop: '48px', }}>Student Menu</div>
-          <div style={{ width: '100%', textAlign: 'center', fontSize: 18, marginBottom: 16 }} >{this.state.userInfo.name}</div>
+        <div className="col-md-4 sidenav" style={{ height: '94vh', paddingLeft: '32px' }}>
+          <div style={{ width: '100%', textAlign: 'center', paddingTop: '48px' }}>Student Menu</div>
+          <div className="input-group" style={{ margin: 0 }}>
+            <span className="input-group-addon">
+              <i className="glyphicon glyphicon-tag" />
+            </span>
+            <input
+              style={{ borderLeftTopRadius: 0, borderLeftBottomRadius: 0 }}
+              type="text"
+              className="form-control"
+              ref="courseNo"
+              placeholder="Course NO. : 88888888"
+              value={this.state.courseNo}
+              onChange={data => this.handleSearch(data)}
+            />
+          </div>
           <Divider />
           <List>
-            <ListItem primaryText="Profile" leftIcon={<ActionUser />} onClick={() => this.changePage(0)} />
-            <ListItem primaryText="Schedule" leftIcon={<ActionSchedule />} onClick={() => this.changePage(1)} />
-            <ListItem primaryText="Course" leftIcon={<ActionToc />} onClick={() => this.changePage(2)} />
-            <ListItem primaryText="Grade" leftIcon={<SocialSchool />} onClick={() => this.changePage(3)} />
-            <ListItem primaryText="Request" leftIcon={<ActionDescription />} onClick={() => this.changePage(4)} />
-            <ListItem primaryText="Payment" leftIcon={<ActionPayment />} onClick={() => this.changePage(5)} />
+            {resultList.map(item => (
+              <ListItem
+                primaryText={item.nname + '  ' + item.fname + '  ' + item.lname}
+                onClick={() => this.handleListClicked(item)}
+              />
+            ))}
           </List>
         </div>
-        <div className="col-md-9" style={{ paddingTop: '16px', paddingRight: '32px' }}>
+        <div className="col-md-8" style={{ paddingRight: '32px' }}>
           <Card>
-            <CardTitle title={this.state.selectedPage.name.toUpperCase()} subtitle={this.state.selectedPage.detail} />
             {
               <CardText style={{ height: '78vh', width: '100%' }}>
-                {this.state.selectedPage.name === 'Profile' && <UserProfile userInfo={this.state.userInfo} />}
-                {this.state.selectedPage.name === 'Course' && <SearchPanel studentCourses={this.state.studentCourses} updateStudentCourses={() => this.getStudentCourses()} />}
-                {this.state.selectedPage.name === 'Schedule' && <Table year={2017} semester={2} courses={this.state.studentCourses} />}
-                {this.state.selectedPage.name === 'Grade' && <Grade courses={this.state.studentCourses} />}
-                {this.state.selectedPage.name === 'Request' && <Request />}
-                {this.state.selectedPage.name === 'Payment' && <Payment />}
+                {/* {this.state.mode === '' && <SearchPanel />} */}
+                {this.state.mode === 'Profile' && (
+                  <UserProfile
+                    userInfo={userInfo}
+                    onModeChange={this.handleModeChanged}
+                    columnData={columnData}
+                  />
+                )}
+                {this.state.mode === 'Edit' && (
+                  <Grade
+                    userInfo={userInfo}
+                    onCancel={this.handleModeChanged}
+                    onSubmit={this.handleEdit}
+                    columnData={columnData}
+                  />
+                )}
               </CardText>
             }
           </Card>
         </div>
       </div>
     );
-  }
+  };
 }
 
 export default DashboardStudent;
